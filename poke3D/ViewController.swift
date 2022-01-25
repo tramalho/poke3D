a09a759ff3ec5ccdbf0c779ae5a028134ccca8fc
tramalho <thiagofacul@gmail.com>
2022-01-25 08:05:07 -0300

    386. How to Add Plane to Card

--- poke3D/ViewController.swift
+++ poke3D/ViewController.swift
@@ -51,28 +51,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+   
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        
+        let physicalSize = imageAnchor.referenceImage.physicalSize
+        
+        let plane = SCNPlane(width: physicalSize.width, height: physicalSize.height)
+        
+        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.7)
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        planeNode.eulerAngles.x = -Float.pi / 2
+        
+        node.addChildNode(planeNode)
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
